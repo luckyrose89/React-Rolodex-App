6de604ff6cbf5b7f76209413288f1454659c9088
avidrutham <divyamathur89@gmail.com>
2020-05-27 16:14:16 +0530

    Updates

---
+++ precache-manifest.151999cfc1883ce0f74d73cd06c71f84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c96c0c3b3629b8e58eb905da612f7d9",
+    "url": "/React-Rolodex-App/index.html"
+  },
+  {
+    "revision": "aae055a44069996adf10",
+    "url": "/React-Rolodex-App/static/css/main.26e037f1.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/React-Rolodex-App/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Rolodex-App/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aae055a44069996adf10",
+    "url": "/React-Rolodex-App/static/js/main.381817cd.chunk.js"
+  },
+  {
+    "revision": "d41c4b34599ef9d0987c",
+    "url": "/React-Rolodex-App/static/js/runtime-main.d62ba48d.js"
+  }
+]);
